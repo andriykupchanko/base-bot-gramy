5324ad7413c9555e92999d13080da5c8e119b22c
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2024-03-09 21:02:56 +0200

    add cath err

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Bot } = require("grammy");
+const { Bot, GrammyError, HttpError } = require("grammy");
 require("dotenv").config();
 
 const botToken = process.env.BOT_TOKEN;
@@ -6,6 +6,18 @@ const botToken = process.env.BOT_TOKEN;
 const bot = new Bot(botToken);
 
 bot.command("start", ctx => ctx.reply("Welcome! Up and running."));
-bot.on("message", ctx => ctx.reply("Got    another message!"));
 
+bot.on("message", ctx => ctx.reply("Got another message!"));
+
+bot.catch(err => {
+  const ctx = err.ctx;
+  console.log(`Error while handling update ${ctx.update.update_id}:`);
+  if (err instanceof GrammyError) {
+    console.error("Client error:", err.description);
+  } else if (err instanceof HttpError) {
+    console.error("Server error:", err.statusCode);
+  } else {
+    console.error("Unknown error:", err);
+  }
+});
 bot.start();
